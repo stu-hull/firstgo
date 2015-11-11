ff6c3de91be193dcf91f34c01de444be2828fb01
Stuart <10s.hull@dsh-pms.com>
2015-11-11 15:33:29 +0000

    examples added

---
+++ ex2.java
@@ -0,0 +1,14 @@
+public class ex2{
+	public static void main(String[] args){
+
+		for (int i = 1; i < 10; i++){
+			System.out.println(i);
+		}
+
+		int p = 2;
+		while (p < 1000){
+			p = p*p;
+			System.out.println(p);
+		}
+	}
+}
---
+++ examples.java
@@ -0,0 +1,9 @@
+public class examples{
+	public static void main(String[] args){
+
+		System.out.println("Hello");
+		System.out.println("World!");
+		int number = 3;
+		System.out.println(number);
+	}
+}
