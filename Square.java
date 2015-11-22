040b96b95dce85de716c4ae2b05737bf8554ca8c
Stuart <10s.hull@dsh-pms.com>
2015-11-22 15:52:12 +0000

    Modified and compiled square.java

--- Square.java
+++ Square.java
@@ -1,12 +1,13 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class Square{
   public static void main(String[] args){
 	  
 	  Scanner input = new Scanner(System.in);
+	  int x = 1;
 	  do {
-		  System.out.println(“Enter a number: “);
-		  double x = input.nextline();
+		  System.out.println("Enter a whole number: ");
+		  x = input.nextInt();
 		  System.out.println(x*x);
 	  } while (x != 0);
 	  
