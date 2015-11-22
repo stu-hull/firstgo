bc8c4a2a351290c37bf2b3781bbb6a1d65d1151e
stu-hull <10s.hull@dsh-pms.com>
2015-11-22 15:38:23 +0000

    Create Square.java

---
+++ Square.java
@@ -0,0 +1,14 @@
+import java.util.scanner;
+
+public class Square{
+  public static void main(String[] args){
+	  
+	  Scanner input = new Scanner(System.in);
+	  do {
+		  System.out.println(“Enter a number: “);
+		  double x = input.nextline();
+		  System.out.println(x*x);
+	  } while (x != 0);
+	  
+	}
+}
