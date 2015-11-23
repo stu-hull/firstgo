9b62a4845ea3910a59a2cd44bbba0cc130b70e1a
Stuart <10s.hull@dsh-pms.com>
2015-11-23 10:06:21 +0000

    Modulo

---
+++ Modulo.java
@@ -0,0 +1,12 @@
+public class Modulo{
+	public static void main(String[] args){
+
+		for (int lenny = 1; lenny <= 20; lenny++){
+			if (lenny % 2 == 0){
+				System.out.println("Even");
+			} else {
+				System.out.println("Odd");
+			}
+		}
+	}
+}
